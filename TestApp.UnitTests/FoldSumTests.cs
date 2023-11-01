c0dcce8e4ccfa763e6614445462d7ae534f39bd3
Daniel Minchev <lbdidko@gmail.com>
2023-11-01 23:48:52 +0200

    new

---
+++ TestApp.UnitTests/FoldSumTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TestApp.Tests;
+
+public class FoldSumTests
+{
+    [TestCase(new int[] { 1, 2, 3, 4 }, "3 7")]
+    [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 }, "5 5 13 13")]
+    [TestCase(new int[] {}, "")]
+    [TestCase(new int[] { 0, 0, 0, 0 }, "0 0")]
+    [TestCase(new int[] { -1, 2, -3, 4 }, "1 1")]
+    public void Test_FoldArray_ReturnsCorrectString(int[] arr, string expected)
+    {
+        // Act
+        string result = FoldSum.FoldArray(arr);
+
+        // Assert
+        Assert.AreEqual(expected, result);
+    }
+}
---
+++ TestApp.UnitTests/LongestIncreasingSubsequenceTests.cs
@@ -0,0 +1,69 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class LongestIncreasingSubsequenceTests
+{
+    [Test]
+    public void Test_GetLis_NullArray_ThrowsArgumentNullException()
+    {
+        // Arrange
+        int[] lis = null;
+
+        // Act & Arrenge
+        Assert.That(() => LongestIncreasingSubsequence.GetLis(lis), Throws.ArgumentNullException);
+    }
+
+    [Test]
+    public void Test_GetLis_EmptyArray_ReturnsEmptyString()
+    {
+        // Arrenge 
+        int[] lis = Array.Empty<int>();
+
+        // Act
+        string output = LongestIncreasingSubsequence.GetLis(lis);
+
+        // Assert
+        Assert.That(output, Is.Empty);
+    }
+
+    [Test]
+    public void Test_GetLis_SingleElementArray_ReturnsElement()
+    {
+        // Arrenge 
+        int[] lis = new int[] {1};
+
+        // Act
+        string output = LongestIncreasingSubsequence.GetLis(lis);
+
+        // Assert
+        Assert.That(output, Is.EqualTo("1")); 
+    }
+
+    [Test]
+    public void Test_GetLis_UnsortedArray_ReturnsLongestIncreasingSubsequence()
+    {
+        // Arrenge 
+        int[] lis = new int[] { 1, 2, 5, 7, 3, 4 };
+
+        // Act
+        string output = LongestIncreasingSubsequence.GetLis(lis);
+
+        // Assert
+        Assert.That(output, Is.EqualTo("1 2 5 7"));
+    }
+
+    [Test]
+    public void Test_GetLis_SortedArray_ReturnsItself()
+    {
+        // Arrenge 
+        int[] lis = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+
+        // Act
+        string output = LongestIncreasingSubsequence.GetLis(lis);
+
+        // Assert
+        Assert.That(output, Is.EqualTo("1 2 3 4 5 6 7"));
+    }
+}
