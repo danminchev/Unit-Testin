aa5edb796ec0bc6b436d601fc85babd888e7d838
Daniel Minchev <lbdidko@gmail.com>
2023-11-04 20:23:59 +0200

    new

---
+++ TestApp.UnitTests/DrumSetTests.cs
@@ -0,0 +1,98 @@
+﻿using NUnit.Framework;
+using System;
+
+using System.Collections.Generic;
+
+namespace TestApp.Tests;
+
+public class DrumSetTests
+{
+    [Test]
+    public void Test_Drum_TerminateCommandNotGiven_ThrowsArgumentException()
+    {   // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new List<int>() { 3, 4 };
+        List<string> commands = new List<string>() { "1", "1" };
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(money, initialQuality, commands));
+    }
+
+    [Test]
+    public void Test_Drum_StringGivenAsCommand_ThrowsArgumentException()
+    {   // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new List<int>() { 3, 4 };
+        List<string> commands = new List<string>() { "1", "1", "string" };
+
+        // Act & Assert
+        Assert.Throws<ArgumentException>(() => DrumSet.Drum(money, initialQuality, commands));
+    }
+
+    [Test]
+    public void Test_Drum_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new List<int>() { 3, 4, };
+        List<string> commands = new() { "1", "1", "Hit it again, Gabsy!" };
+
+        string expected = "1 2\nGabsy has 100.00lv.";
+
+        // Act
+        string output = DrumSet.Drum(money, initialQuality, commands);
+
+        // Assert
+        Assert.That(output, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_Drum_BalanceZero_WithOneDrumLeftOver_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 0;
+        List<int> initialQuality = new List<int>() { 1, 2 };
+        List<string> commands = new List<string>() { "1", "1", "2", "Hit it again, Gabsy!" };
+
+        string expected = "\nGabsy has 0.00lv.";
+
+        // Act
+        string output = DrumSet.Drum(money, initialQuality, commands);
+
+        //Assert
+        Assert.That(expected, Is.EqualTo(output));
+    }
+
+    [Test]
+    public void Test_Drum_NotEnoughBalance_RemovesAllDrums_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 9;
+        List<int> initialQuality = new List<int>() { 3, 3 };
+        List<string> commands = new List<string>() { "1", "1", "2", "Hit it again, Gabsy!" };
+
+        string expected = "\nGabsy has 9.00lv.";
+        // Act
+        string output = DrumSet.Drum(money, initialQuality, commands);
+
+        //Assert
+        Assert.That(expected, Is.EqualTo(output));
+    }
+
+    [Test]
+    public void Test_Drum_BrokenDrum_ReturnsCorrectQualityAndAmount()
+    {
+        // Arrange
+        decimal money = 100;
+        List<int> initialQuality = new List<int>() { 3, 10 };
+        List<string> commands = new List<string>() { "2", "2", "Hit it again, Gabsy!" };
+
+        string expected = "3 6\nGabsy has 91.00lv.";
+
+        // Act
+        string output = DrumSet.Drum(money, initialQuality, commands);
+
+        //Assert
+        Assert.That(expected, Is.EqualTo(output));
+    }
+}
---
+++ TestApp.UnitTests/ExtractFileTests.cs
@@ -0,0 +1,72 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace TestApp.Tests;
+
+public class ExtractFileTests
+{
+    [Test]
+    public void Test_GetFile_NullPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string path = null;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(path));
+    }
+
+    [Test]
+    public void Test_GetFile_EmptyPath_ThrowsArgumentNullException()
+    {
+        // Arrange
+        string path = string.Empty;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => ExtractFile.GetFile(path));
+    }
+
+    [Test]
+    public void Test_GetFile_ValidPath_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string path = @"C:\folder\file.txt";
+
+        string expected = "File name: file\nFile extension: txt";
+
+        // Act
+        string result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithNoExtension_ReturnsFileNameOnly()
+    {
+        // Arrange
+        string path = @"D:\another_folder\document";
+
+        string expected = "File name: document";
+
+        // Act
+        string result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Test_GetFile_PathWithSpecialCharacters_ReturnsFileNameAndExtension()
+    {
+        // Arrange
+        string path = @"E:\data\my_file 123.docx";
+
+        string expected = "File name: my_file 123\nFile extension: docx";
+
+        // Act
+        string result = ExtractFile.GetFile(path);
+
+        // Assert
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
